a6bd8ed29b87c72db3e4629db50e377d9948b2bd
Martin Mihaylov <martinmihaylov55@gmail.com>
2020-03-24 23:47:30 -0400

    Added crappy rail component (will fix later)

--- hhg/src/App.js
+++ hhg/src/App.js
@@ -2,22 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Rail extends React.Component {
+  render() {
+    return (
+		<b>|----LMAO-RAIL----|</b>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+		<Rail />
+		<Rail />
+		<Rail />
       </header>
     </div>
   );
