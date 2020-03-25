4597a06e5262f4a59277a55a3e4adb32c191610f
Martin Mihaylov <martinmihaylov55@gmail.com>
2020-03-25 17:52:06 -0400

    Resolved merge conflicts

--- hhg/src/App.js
+++ hhg/src/App.js
@@ -3,51 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import Rails from './components/Rails'
 
-<<<<<<< HEAD
-class Rail extends React.Component {
-  render() {
-    return (
-		<img src = {require("./assets/rail.png")} alt="" className="img-responsive" />
-    );
-  }
-}
-
-
-
-class RailLine extends React.Component {
-  
-  createLine = (length) => {
-	let line = []
-	for (let i = 0; i < length; i++) {
-		line.push(<Rail />)
-	}
-	return line
-  }
-  
-  render() {
-    return (
-		<div className = "RowOfRails">
-		    {this.createLine(12)}
-		</div>
-    );
-  }
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-	  <RailLine />
-      </header>
-    </div>
-=======
-
-
-
 function App() {
   return (
 	<Rails />
->>>>>>> master
   );
 }
 
