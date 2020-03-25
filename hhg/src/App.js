20bc6090f5276e4d2faa1e9692af8348acf09992
Nahzilian <50055124+Nahzilian@users.noreply.github.com>
2020-03-25 15:37:20 -0400

    Added number of rails input

--- hhg/src/App.js
+++ hhg/src/App.js
@@ -1,40 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Rails from './components/Rails'
 
-class Rail extends React.Component {
-  render() {
-    return (
-		<img src = {require("./assets/rail.png")} alt="" className="img-responsive" />
-    );
-  }
-}
-
-function createLine(length){
-	let line = []
-	for (let i = 0; i < length; i++) {
-		line.push(<Rail />)
-	}
-	return line
-  }
 
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-		<div className = "RowOfRails">
-			{createLine(15)}
-		</div>
-		<div className = "RowOfRails">
-			{createLine(20)}
-		</div>
-		<div className = "RowOfRails">
-			{createLine(10)}
-		</div>
-      </header>
-    </div>
+	<Rails />
   );
 }
 
---
+++ hhg/src/components/Rails.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+
+import RailImage from '../assets/rail.png'
+
+export class Rails extends Component {
+    state = {
+        number: ''
+    }
+    
+    change = (e) =>{
+        this.setState(
+            {
+                [e.target.name]:e.target.value
+            }
+        )
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+    createLine(length){
+        let line = []
+        for (let i = 0; i < length; i++) {
+            line.push(<img src = {RailImage} alt="" className="img-responsive" />)
+        }
+        return line
+      }
+
+
+    
+    createRail = (num) => {
+        let rails = []
+        for (let i = 0; i < num; i++) {
+          rails.push(<div>{this.createLine(10)}</div>)
+        }
+        return rails
+      }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <label htmlFor="fname">Number of rails:</label><br/>
+                    <input 
+                        type = "text"
+                        name = "number" 
+                        placeholder = "Enter here ..." 
+                        value ={this.state.number} 
+                        onChange={e => this.change(e)}>
+                    </input>
+                    <br/>
+
+                    {/** 
+                    <button 
+                    type = "submit" 
+                    onClick = {(e) => this.onSubmit()} 
+                    >
+                        Submit
+                    </button>
+                    */}
+                </form>
+                <div>
+                    {this.createRail(this.state.number)}
+                </div>
+            </div>
+            
+        )
+    }
+}
+
+export default Rails
+
+
+
+  
