b62a305f5f04caf5eae91dd5548394ff6b3104e0
Martin Mihaylov <martinmihaylov55@gmail.com>
2020-03-25 22:02:40 -0400

    Auto stash before merge of "master" and "origin/master"
    Added arrow key override for Player

--- hhg/src/components/Player.js
+++ hhg/src/components/Player.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react'
-
+import ArrowKeysReact from 'arrow-keys-react';
 import PlayerImage from '../assets/dude.png'
 
 export class Player extends Component {
-
+	ArrowKeysReact.config({
+      left: () => {
+        console.log('left key detected.');
+      },
+      right: () => {
+        console.log('right key detected.');
+      },
+      up: () => {
+        console.log('up key detected.');
+      },
+      down: () => {
+        console.log('down key detected.');
+      }
+    });
     render() {
         return (
+<<<<<<< Updated upstream
             <div>
                 
+=======
+            <div {...ArrowKeysReact.events} tabIndex = "1">
+				<img src = {PlayerImage} alt="" className="img-responsive" />
+>>>>>>> Stashed changes
             </div>
-            
         )
     }
 }
