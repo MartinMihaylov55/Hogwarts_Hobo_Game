661c75ad5f2272e9fdd00923f555e95ef0c46e3a
Martin Mihaylov <martinmihaylov55@gmail.com>
2020-03-25 21:40:59 -0400

    Added player class TODO

---
+++ hhg/src/components/Player.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import PlayerImage from '../assets/dude.png'
+
+export class Player extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+            
+        )
+    }
+}
+
+export default Rails
