bae4bd2761d0c43a7d725246f1b2bf06e99b0368
david <david.peprah@ryerson.ca>
2020-04-10 19:56:33 -0400

    Update Hobo.py

--- analysis/Hobo.py
+++ analysis/Hobo.py
@@ -4,10 +4,6 @@ from Train import Train
 class Hobo:
     def __init__(self, position):
         self.position = position
-        self.hint = []
-
-    def addInfo(self, position):
-        self.paperPlanes[self.position] = position
 
     def getPosition(self):
         return self.position
