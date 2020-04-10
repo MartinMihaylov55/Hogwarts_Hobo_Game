326633c8909bee34492ee65fd7c3f5c49821028c
DavidPep <45982650+DavidPep@users.noreply.github.com>
2020-04-09 23:04:16 -0400

    Track

--- analysis/Train.py
+++ analysis/Train.py
@@ -1,4 +1,9 @@
 class Train:
     def __init__(self, position):
         self.position = position
+    def __init__(self, track):
+        self.track = track
+    def getPosition():
+        return self.postion
+    
 
