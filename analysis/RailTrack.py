32cbd9d875d648a3ae84519ffdf9ea67d40839dc
Martin Mihaylov <martinmihaylov55@gmail.com>
2020-04-09 20:30:48 -0400

    Started Python backend

---
+++ analysis/RailTrack.py
@@ -0,0 +1,5 @@
+class RailTrack: 
+    def __init__(self, position):
+        self.position = position
+    def __str__(self):
+        return "RailTrack: position " + str(self.position)
--- hhg/src/App.js
+++ hhg/src/App.js
@@ -11,7 +11,6 @@ function App() {
 		  	<Rails />
 			<Player />
 			<Train />
-
 		</Fragment>
 	
   );
