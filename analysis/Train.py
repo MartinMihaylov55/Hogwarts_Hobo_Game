dbbbda83c9b7571a2695f0ce10f33ac5739a83da
DavidPep <45982650+DavidPep@users.noreply.github.com>
2020-04-10 07:41:43 -0400

    add move

--- analysis/Train.py
+++ analysis/Train.py
@@ -2,6 +2,8 @@ class Train:
     def __init__(self, position, track):
         self.position = position
         self.track = track
+    def move(self, steps):
+        self.position += steps
     def getPosition():
         return self.postion
     def getTrack():
