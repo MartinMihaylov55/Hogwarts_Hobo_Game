4aa7c4c1f2d49faf76f143b9b223541d47bb120c
superMsp10 <pandeymahan34@gmail.com>
2020-04-10 00:30:08 -0400

    Program Design

--- analysis/Analyze.py
+++ analysis/Analyze.py
@@ -6,3 +6,12 @@ elapsedTime = 0
 
 t = Tunnel(5)
 print("Generated RailTrack object successfully, " + str(t))
+
+# Take parameter as intput from stdin
+# Game Loop run for n iterations
+# In each step:
+# We ask our player if they want to move (potential algorithm: move to random track)
+# We ask benchmark player if they want to move
+# Move trains, check for collisions
+# Sum the failiures
+# After all iterations are done return return the percentage of successes for each player
