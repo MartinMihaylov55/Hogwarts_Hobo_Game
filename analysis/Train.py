7833f9cff793174a238da8c7dca6bc86db680cfb
DavidPep <45982650+DavidPep@users.noreply.github.com>
2020-04-10 13:59:20 -0400

    Update Train.py

--- analysis/Train.py
+++ analysis/Train.py
@@ -1,14 +1,11 @@
 class Train:
-    def __init__(self, position, track, speed,time):
+    def __init__(self, position, track, spendTime, nextSpendTime):
         self.position = position
-        self.track = track
-    def move(self):
-        self.position += 1
-    def getPosition(self):
-        return self.postion
-    def getTrack(self):
-        return self.track
-
+        self.speed = speed
+        self.spendTime = spendTime
+        self.nextSpendTime = nextSpendTime
+        
+    
 #Do we need the time?
 #We could've just use the speed and the distance to get the time
 #Is track neccessary
