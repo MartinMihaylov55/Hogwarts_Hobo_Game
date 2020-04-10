59b0b0e877a1931badf8cd14ea801b8351dd639b
pkandic4 <petar.kandic@ryerson.ca>
2020-04-09 23:15:12 -0400

    Outline of test cases

---
+++ TestCases.py
@@ -0,0 +1,24 @@
+from analysis import Hobo
+from analysis import Analyze
+from analysis import RailTrack
+from analysis import Train
+from analysis import Tunnel
+##Compiler complains if from analysis import * used
+import unittest
+
+class Test(unittest.TestCase):
+
+    def testhobo(self):
+        hoboInstance = Hobo
+
+    def testanalyze(self):
+        analyzeInstance = Analyze
+
+    def testrailtrack(self):
+        railtrackInstance = RailTrack
+
+    def traintest(self):
+        trainInstance = Train
+
+    def tunnelTest(self):
+        tunnelInstance = Tunnel
